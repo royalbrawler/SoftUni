137c92ea4f6c6ad6a2133b5fe748e9c84d1180a5
royalbrawler <farciebe@gmail.com>
2016-12-04 19:47:26 +0200

    asdawd

--- ExperimentsHERE/ExperimentsHERE/Program.cs
+++ ExperimentsHERE/ExperimentsHERE/Program.cs
@@ -11,24 +11,25 @@ namespace ExperimentsHERE
     {
         static void Main(string[] args)
         {
-            int hour = int.Parse(Console.ReadLine());
-            int minutes = int.Parse(Console.ReadLine());
-            minutes += 15;
+            //int hour = int.Parse(Console.ReadLine());
+            //int minutes = int.Parse(Console.ReadLine());
+            //minutes += 15;
 
-            if (minutes >= 60)
-            {
-                hour++;
-                minutes -= 60;
-            }
-            if (hour >= 24)
-            {
-                hour -= 24;
-            }
+            //if (minutes >= 60)
+            //{
+            //    hour++;
+            //    minutes -= 60;
+            //}
+            //if (hour >= 24)
+            //{
+            //    hour -= 24;
+            //}
 
-            if (minutes < 10)
-                Console.WriteLine(hour + ":0" + minutes);
-            else
-                Console.WriteLine(hour + ":" + minutes);
+            //if (minutes < 10)
+            //    Console.WriteLine(hour + ":0" + minutes);
+            //else
+            //    Console.WriteLine(hour + ":" + minutes);
+            Console.WriteLine(Math.Round(1.2457, 2));
         }
     }
 }
---
+++ ExperimentsHERE/exam/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skumria = double.Parse(Console.ReadLine());
+            double caca = double.Parse(Console.ReadLine());
+            double palamud = double.Parse(Console.ReadLine());
+            double safrid = double.Parse(Console.ReadLine());
+            double midi = double.Parse(Console.ReadLine());
+
+            double Cenapalamud = skumria * 1.60;
+            palamud *= Cenapalamud;
+            double CenaSafrid = caca * 1.80;
+            safrid *= CenaSafrid;
+            midi = midi * 7.50;
+            double cena = palamud + safrid + midi;
+
+            Console.WriteLine(Math.Round(cena, 2));
+        }
+    }
+}
