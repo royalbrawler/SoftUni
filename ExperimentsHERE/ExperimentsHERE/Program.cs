bcf6518cfdfcaa6ad9a33baa938fc2ef56015589
royalbrawler <farciebe@gmail.com>
2017-01-22 01:06:03 +0200

    basics presentations

--- ExperimentsHERE/ExperimentsHERE/Program.cs
+++ ExperimentsHERE/ExperimentsHERE/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace ExperimentsHERE
 {
@@ -6,33 +7,8 @@ namespace ExperimentsHERE
     {
         static void Main(string[] args)
         {
-            int magnol = int.Parse(Console.ReadLine());
-            double triTona = 0;
-            double fourTona = 0;
-            double twelveTona = 0;
-
-            for (int i = 0; i < magnol; i++)
-            {
-                double asd = double.Parse(Console.ReadLine());
-                if (asd <= 3)
-                {
-                    triTona += asd;
-                }
-                else if (asd > 3 && asd <= 11)
-                {
-                    fourTona += asd;
-                }
-                else
-                {
-                    twelveTona += asd;
-                }
-            }
-            double Tonove = triTona + fourTona + twelveTona;
-            double srednaCena = ((triTona * 200) + (fourTona * 175) + (twelveTona * 120)) / Tonove;
-            Console.WriteLine("{0:F2}", srednaCena);
-            Console.WriteLine("{0:F2}%", triTona / Tonove * 100);
-            Console.WriteLine("{0:F2}%", fourTona / Tonove * 100);
-            Console.WriteLine("{0:F2}%", twelveTona / Tonove * 100);
+            double asd = 10 / 0.0;
+            Console.WriteLine(asd);
         }
     }
 }
---
+++ zzzzzSOFTUNI presentations/basics/Solution20november2016EVENING/Problem01HousePrice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousePrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var smallestRoom = double.Parse(Console.ReadLine());
+            var kitchen = double.Parse(Console.ReadLine());
+            var price = double.Parse(Console.ReadLine());
+
+            var secondRoom = smallestRoom * 1.1;
+            var thirdRoom = secondRoom*1.1;
+            var bathroom = smallestRoom/2;
+
+            var area = smallestRoom + kitchen + thirdRoom + bathroom + secondRoom;
+            area = area + (5*area)/100;
+
+            var finalPrice = area*price;
+
+            Console.WriteLine("{0:F2}",finalPrice);
+        }
+    }
+}
